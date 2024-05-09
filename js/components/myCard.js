e750f432f5734335f1723510160635796f7da723
DavidJJulio <158339997+DavidJJulio@users.noreply.github.com>
2024-05-08 21:52:59 -0500

    feat: :construction: From 0

--- js/components/myCard.js
+++ js/components/myCard.js
@@ -1,46 +1,21 @@
-import {
-    getSpainClients
-} from "../module/clients"
-
-
-
-
-
-
-
-
-export class myCard extends HTMLElement {
-    constructor(){
-        this.attachShadow({mode: "open"});
-        this.shadowRoot.innerHTML = /*html*/`
-        <link rel="stylesheet" href="../css/myCard.css">`
-        super();
-    }
-
-    async getSpainClientsDesign(){
-        let data = await getSpainClients();
-        data.forEach(val =>{
-            this.shadowRoot.innerHTML += /*html*/`
-            <div class="report__card">
+export class Mycard extends HTMLElement {
+constructor(){
+    super();
+    this.attachShadow({mode: "open"})
+    this.shadowRoot.innerHTML += /*html*/`
+                <link rel="stylesheet" href="../css/myCard.css">
+                <div class="report__card">
                     <div class="card__title">
-                        <div>${val.client_name}</div>
+                        <div>prueba</div>
                     </div>
                     <div class="card__body">
                         <div class="body__marck">
-                            <p>adadasdadadadsd</p>
-                            <p>dadssadsadadsadsad</p>
+                            <p><b>Nombre del empleado: </b>prueba</p>
+                            <p><b>Ciudad: </b>prueba</p>
                         </div>
                     </div>
                 </div>
-            `
-        })
-    }
-
-    static get observedAttributes() {
-        return ["logic"];
-    }
-    attributeChangedCallback(name, old, now) {
-        if(name=="logic" && now=="client_6") this.getSpainClientsDesign()
-    }
+    `
+}
 
 }
--- js/components/myDetails.js
+++ js/components/myDetails.js
@@ -1,7 +1,4 @@
 export class Mydetails extends HTMLElement{
-    myCard
-    p
-    details
     constructor(){
         super();
         this.attachShadow({mode: "open"});
@@ -11,7 +8,7 @@ export class Mydetails extends HTMLElement{
                 <summary>
                     <div class="details__description">Campus: </div>
                     <div class="details__container">
-                        <p></p>
+                        <p>Consulta x</p>
                     </div>
                 </summary>
                 <div class="report__container">
@@ -19,17 +16,8 @@ export class Mydetails extends HTMLElement{
                 </div>
             </details>
         `;
-        this.myCard = this.shadowRoot.querySelector("my-card")
-        this.p = this.shadowRoot.querySelector("p");
-        this.details = this.shadowRoot.querySelector("details")
+        
     }
 
-    static get observedAttributes() {
-        return ["logic", "text"];
-    }
-    attributeChangedCallback(name, old, now) {
-        if(name == "logic") Mydetails.query = now
-        if(name == "text") this.p.textContent = now
-    }
 }
 
