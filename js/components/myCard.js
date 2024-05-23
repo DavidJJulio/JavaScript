4a6d128d9384b20712aaffe89db302b1ff1af1e4
DavidJJulio <158339997+DavidJJulio@users.noreply.github.com>
2024-05-23 02:34:54 -0500

    feat: :white_check_mark: Adding some consultations

--- js/components/myCard.js
+++ js/components/myCard.js
@@ -159,7 +159,7 @@ export class Mycard extends HTMLElement{
             this.shadowRoot.innerHTML += /* html */`
                 <div class="report__card">
                     <div class="card__title">
-                            <div>Empleados que tengan oficina en Fuenlabrada</div>
+                            <div>Clientes con pedido retrasado</div>
                         </div>
                             <div class="card__body">
                                 <div class="body__marck">
@@ -177,7 +177,7 @@ export class Mycard extends HTMLElement{
             this.shadowRoot.innerHTML += /* html */`
                 <div class="report__card">
                     <div class="card__title">
-                            <div>Empleados que tengan oficina en Fuenlabrada</div>
+                            <div>Nombre y empleado de clientes que no hayan realizado pago</div>
                         </div>
                             <div class="card__body">
                                 <div class="body__marck">
@@ -196,7 +196,7 @@ export class Mycard extends HTMLElement{
             this.shadowRoot.innerHTML += /* html */`
                 <div class="report__card">
                     <div class="card__title">
-                            <div>Empleados que tengan oficina en Fuenlabrada</div>
+                            <div>Empleados sin cliente </div>
                         </div>
                             <div class="card__body">
                                 <div class="body__marck">
@@ -216,7 +216,7 @@ export class Mycard extends HTMLElement{
             this.shadowRoot.innerHTML += /* html */`
                 <div class="report__card">
                     <div class="card__title">
-                            <div>Empleados que tengan oficina en Fuenlabrada</div>
+                            <div>Oficinas y su ciudad</div>
                         </div>
                             <div class="card__body">
                                 <div class="body__marck">
@@ -234,7 +234,7 @@ export class Mycard extends HTMLElement{
             this.shadowRoot.innerHTML += /* html */`
                 <div class="report__card">
                     <div class="card__title">
-                            <div>Empleados que tengan oficina en Fuenlabrada</div>
+                            <div>Oficinas de España</div>
                         </div>
                             <div class="card__body">
                                 <div class="body__marck">
@@ -252,7 +252,7 @@ export class Mycard extends HTMLElement{
             this.shadowRoot.innerHTML += /* html */`
                 <div class="report__card">
                     <div class="card__title">
-                            <div>Empleados que tengan oficina en Fuenlabrada</div>
+                            <div>Oficinas de empleados</div>
                         </div>
                             <div class="card__body">
                                 <div class="body__marck">
@@ -269,6 +269,180 @@ export class Mycard extends HTMLElement{
                         `;
                     });
     }
+    async getEmployeesDiff_from_SpecificPositionDesign(){
+        let data = await getEmployeesDiff_from_SpecificPosition()
+        data.forEach(val => {
+            this.shadowRoot.innerHTML += /* html */`
+                <div class="report__card">
+                    <div class="card__title">
+                            <div>Empleados de una posicion especifica</div>
+                        </div>
+                            <div class="card__body">
+                                <div class="body__marck">
+                                    <p><b>Nombre del Empleado: </b>${val.name}</p>
+                                    <p><b>Apellido del empleado: </b>${val.employee_lastname1}</p>
+                                    <p><b>Apellido del empleado: </b>${val.employee_lastname2}</p>
+                                    <p><b>Posicion: </b>${val.position}</p>
+                                </div>
+                        </div>
+                </div>
+                        `;
+                    });
+    }
+    async getEmployeeOfTheClientDesign(){
+        let data = await getEmployeeOfTheClient()
+        data.forEach(val => {
+            this.shadowRoot.innerHTML += /* html */`
+                <div class="report__card">
+                    <div class="card__title">
+                            <div>Empleados de un cliente</div>
+                        </div>
+                            <div class="card__body">
+                                <div class="body__marck">
+                                    <p><b>Nombre del Cliente: </b>${val.name}</p>
+                                    <p><b>Nombre del Empleado: </b>${val.employee_name}</p>
+                                    <p><b>Apellido del empleado: </b>${val.employee_lastname1}</p>
+                                    <p><b>Apellido del empleado: </b>${val.employee_lastname2}</p>
+                                </div>
+                        </div>
+                </div>
+                        `;
+                    });
+    }
+    async getRequestStatusDesign(){
+        let data = await getRequestStatus()
+        data.forEach(val => {
+            this.shadowRoot.innerHTML += /* html */`
+                <div class="report__card">
+                    <div class="card__title">
+                            <div>Estados de un pedido</div>
+                        </div>
+                            <div class="card__body">
+                                <div class="body__marck">
+                                    <p><b>Estado: </b>${val}</p>
+                                </div>
+                        </div>
+                </div>
+                        `;
+                    });
+    }
+    async getClientsCodeFromSpecificDateDesign(){
+        let data = await getClientsCodeFromSpecificDate()
+        data.forEach(val => {
+            this.shadowRoot.innerHTML += /* html */`
+                <div class="report__card">
+                    <div class="card__title">
+                            <div>Codigo de clientes que realizaron algun pago en 2008</div>
+                        </div>
+                            <div class="card__body">
+                                <div class="body__marck">
+                                    <p><b>Cliente: </b>${val}</p>
+                                </div>
+                        </div>
+                </div>
+                        `;
+                    });
+    }
+    async getRetardedDeliveriesDesign(){
+        let data = await getRetardedDeliveries()
+        data.forEach(val => {
+            this.shadowRoot.innerHTML += /* html */`
+                <div class="report__card">
+                    <div class="card__title">
+                            <div>Pedidos retardados</div>
+                        </div>
+                            <div class="card__body">
+                                <div class="body__marck">
+                                    <p><b>Codigo del cliente: </b>${val.code_client}</p>
+                                    <p><b>Codigo del pedido: </b>${val.code_request}</p>
+                                    <p><b>Fecha esperada: </b>${val.date_wait}</p>
+                                    <p><b>Fecha de entrega: </b>${val.date_delivery}</p>
+                                </div>
+                        </div>
+                </div>
+                        `;
+                    });
+    }
+    async getRightDeliveriesDesign(){
+        let data = await getRightDeliveries()
+        data.forEach(val => {
+            this.shadowRoot.innerHTML += /* html */`
+                <div class="report__card">
+                    <div class="card__title">
+                            <div>Pedidos adelantados</div>
+                        </div>
+                            <div class="card__body">
+                                <div class="body__marck">
+                                    <p><b>Codigo del cliente: </b>${val.code_client}</p>
+                                    <p><b>Codigo del pedido: </b>${val.code_request}</p>
+                                    <p><b>Fecha esperada: </b>${val.date_wait}</p>
+                                    <p><b>Fecha de entrega: </b>${val.date_delivery}</p>
+                                </div>
+                        </div>
+                </div>
+                        `;
+                    });
+    }
+    async getRejectedDeliveriesFromDateDesign(){
+        let data = await getRejectedDeliveriesFromDate()
+        data.forEach(val => {
+            this.shadowRoot.innerHTML += /* html */`
+                <div class="report__card">
+                    <div class="card__title">
+                            <div>Pedidos rechazados del 2009</div>
+                        </div>
+                            <div class="card__body">
+                                <div class="body__marck">
+                                    <p><b>Estatus: </b>${val.status}</p>
+                                    <p><b>Codigo del pedido: </b>${val.code_request}</p>
+                                    <p><b>Fecha esperada: </b>${val.date_wait}</p>
+                                    <p><b>Fecha de entrega: </b>${val.date_delivery}</p>
+                                </div>
+                        </div>
+                </div>
+                        `;
+                    });
+    }
+    async getJanuaryDeliveriesDesign(){
+        let data = await getJanuaryDeliveries()
+        data.forEach(val => {
+            this.shadowRoot.innerHTML += /* html */`
+                <div class="report__card">
+                    <div class="card__title">
+                            <div>Pedidos de enero</div>
+                        </div>
+                            <div class="card__body">
+                                <div class="body__marck">
+                                    <p><b>Estatus: </b>${val.status}</p>
+                                    <p><b>Codigo del pedido: </b>${val.code_request}</p>
+                                    <p><b>Fecha esperada: </b>${val.date_wait}</p>
+                                    <p><b>Fecha de entrega: </b>${val.date_delivery}</p>
+                                </div>
+                        </div>
+                </div>
+                        `;
+                    });
+    }
+    async getPaymentsPaypalByDateDesign(){
+        let data = await getPaymentsPaypalByDate()
+        data.forEach(val => {
+            this.shadowRoot.innerHTML += /* html */`
+                <div class="report__card">
+                    <div class="card__title">
+                            <div>Id de pagos por Paypal del 2008</div>
+                        </div>
+                            <div class="card__body">
+                                <div class="body__marck">
+                                    <p><b>Pago: </b>${val.payment}</p>
+                                    <p><b>Id de la transaccion: </b>${val.Id_transaction}</p>
+                                    <p><b>Fecha del pago: </b>${val.date_payment}</p>
+                                    <p><b>Total: </b>${val.total}</p>
+                                </div>
+                        </div>
+                </div>
+                        `;
+                    });
+    }
     
     static get observedAttributes(){
         return ["logic"]
@@ -278,13 +452,22 @@ export class Mycard extends HTMLElement{
             if (now === "client_1") await this.getSpainClientsDesign();
             if (now === "client_2") await this.getClientsFromCityBySalesManagerCodeDesign();
             if (now === "client_3") await this.getRetardedDeliveryClientsDesign();
+            if (now === "client_4") await this.getEmployeeOfTheClientDesign();
             if (now === "employee_1") await this.getInfoEmployeesByBossDesign();
             if (now === "employee_2") await this.getBossDesign();
             if (now === "employee_3") await this.getNameByClientDesign();
             if (now === "employee_4") await this.getEmployeesWithoutClientsDesign();
+            if (now === "employee_5") await this.getEmployeesDiff_from_SpecificPositionDesign();
             if (now === "offices_1") await this.getOfficesbyCityDesign();
             if (now === "offices_2") await this.getOfficesFromSpainDesign();
             if (now === "offices_3") await this.getOfficesByEmployeesDesign();
+            if (now === "payments_1") await this.getClientsCodeFromSpecificDateDesign();
+            if (now === "payments_2") await this.getPaymentsPaypalByDateDesign();
+            if (now === "requests_1") await this.getRequestStatusDesign();
+            if (now === "requests_2") await this.getRetardedDeliveriesDesign();
+            if (now === "requests_3") await this.getRightDeliveriesDesign();
+            if (now === "requests_4") await this.getRejectedDeliveriesFromDateDesign();
+            if (now === "requests_5") await this.getJanuaryDeliveriesDesign();
 
 
         }
--- js/report.js
+++ js/report.js
@@ -20,6 +20,7 @@ button.forEach(val=>{
         <my-details logic ="client_1" text ="6. Devuelve un listado con el nombre de los todos los clientes españoles. "></my-details>
         <my-details logic ="client_2" text ="16. Devuelve un listado con todos los clientes que sean de la ciudad de Madrid y cuyo representante de ventas tenga el código de empleado 11 o 30. "></my-details>
         <my-details logic ="client_3" text ="MULTITABLA 10. Devuelve el nombre de los clientes a los que no se les ha entregado a tiempo un pedido. "></my-details>
+        <my-details logic ="client_4" text ="1. Obtén un listado con el nombre de cada cliente y el nombre y apellido de su representante de ventas. "></my-details>
         `
     }
     if(e.target.innerHTML=="employees"){
@@ -28,6 +29,7 @@ button.forEach(val=>{
             <my-details logic ="employee_2" text ="4. Devuelve el nombre del puesto, nombre, apellidos y email del jefe de la empresa."></my-details>
             <my-details logic ="employee_3" text ="MULTITABLA 3. Muestra el nombre de los clientes que **no** hayan realizado pagos junto con el nombre de sus representantes de ventas."></my-details>
             <my-details logic ="employee_4" text ="5. Devuelve un listado que muestre solamente los empleados que no tienen un cliente asociado."></my-details>
+            <my-details logic ="employee_5" text ="5. Devuelve un listado con el nombre, apellidos y puesto de aquellos empleados que no sean representantes de ventas."></my-details>
             
         `
     }
@@ -48,7 +50,8 @@ button.forEach(val=>{
 
     if(e.target.innerHTML=="payments"){
         report__details.innerHTML = /*html*/`
-            <h1>Aun no estan estas consultas</h1>
+        <my-details logic ="payments_1" text ="8. Devuelve un listado con el código de cliente de aquellos clientes que realizaron algún pago en 2008. Tenga en cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos. "></my-details>
+        <my-details logic ="payments_2" text ="13. Devuelve un listado con todos los pagos que se realizaron en el año 2008 mediante Paypal. Ordene el resultado de mayor a menor."></my-details>
         `
     }
 
@@ -66,10 +69,15 @@ button.forEach(val=>{
 
     if(e.target.innerHTML=="requests"){
         report__details.innerHTML = /*html*/`
-            <h1>Aun no estan estas consultas</h1>
+        <my-details logic ="requests_1" text ="7. Devuelve un listado con los distintos estados por los que puede pasar un pedido."></my-details>
+        <my-details logic ="requests_2" text ="9. Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos que no han sido entregados a tiempo."></my-details>
+        <my-details logic ="requests_3" text ="10. Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos cuya fecha de entrega ha sido al menos dos días antes de la fecha esperada."></my-details>
+        <my-details logic ="requests_4" text ="11. Devuelve un listado de todos los pedidos que fueron **rechazados** en 2009."></my-details>
+        <my-details logic ="requests_5" text ="12. Devuelve un listado de todos los pedidos que han sido **entregados** en el mes de enero de cualquier año."></my-details>
         `
     }
     })
-    
+
     
 })
+
